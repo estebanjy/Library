a934819ae52978503046508d4e7c0cf98d7e4449
ututogit <romeo.mike@protonmail.com>
2023-11-07 16:10:23 -0300

    Update Registro.py

--- Registro.py
+++ Registro.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QMessageBox, QPushButton, QLabel, QLineEdit)
+from PyQt5.QtWidgets import (QApplication, QWidget, QMessageBox, QButtonGroup, QPushButton, QLabel, QLineEdit)
 from PyQt5.QtGui import QFont, QPixmap
+from PyQt5.Qt import QRadioButton
 
 
 class CrearNuevoUsuario(QWidget):
@@ -11,17 +12,18 @@ class CrearNuevoUsuario(QWidget):
 
     def inicializarUI(self):
         # Inicializa la ventana y muestra su contenido a la pantalla
-        self.setGeometry(100, 100, 400, 400)
+        self.setGeometry(100, 100, 400, 500)
         self.setWindowTitle('Registrar nuevo usuario')
-        self.RecopilarInformacion()
+        self.recopilarInformacion()
         self.show()
 
-    def RecopilarInformacion(self):
+    def recopilarInformacion(self):
         """
         Crea los widgets para recopilar información para crear una nueva cuenta
         Etiqueta de Nuevo Usuario
         """
         icono_nuevo_usuario = "imagenes/nuevo_usuario.png"
+        
         try:
             with open(icono_nuevo_usuario):
                 nuevo_usuario = QLabel(self)
@@ -36,35 +38,72 @@ class CrearNuevoUsuario(QWidget):
         etiqueta_login.move(120, 15)
         etiqueta_login.setFont(QFont('Arial', 20))
         
-        # Etiquetas de nombre de usuario y nombre completo
-        etiqueta_login = QLabel("Nombre de usuario:", self)
-        etiqueta_login.move(35, 180)
+        # Etiquetas de nombre de usuario
+        etiqueta_nombre = QLabel("Nombre de usuario:", self)
+        etiqueta_nombre.move(35, 180)
         self.nombre = QLineEdit(self)
         self.nombre.move(165, 180)
         self.nombre.resize(200, 20)
-        etiqueta_login = QLabel("Nombre completo:", self)
-        etiqueta_login.move(35, 210)
-        nombre = QLineEdit(self)
-        nombre.move(165, 210)
-        nombre.resize(200, 20)
         
+        # Etiqueta documento
+        etiqueta_dni = QLabel("Documento/DNI:", self)
+        etiqueta_dni.move(35, 210)
+        self.dni = QLineEdit(self)
+        self.dni.move(165, 210)
+        self.dni.resize(200, 20)
+        
+        # Etiqueta nro.de telefono
+        etiqueta_tel = QLabel("Teléfono/Cel.:", self)
+        etiqueta_tel.move(35, 240)
+        self.tel = QLineEdit(self)
+        self.tel.move(165, 240)
+        self.tel.resize(200, 20)
+        
+        # Etiqueta domicilio
+        etiqueta_domicilio = QLabel("Domicilio:", self)
+        etiqueta_domicilio.move(35, 270)
+        self.domicilio = QLineEdit(self)
+        self.domicilio.move(165, 270)
+        self.domicilio.resize(200, 20)
+        
+        # Etiqueta fecha de nacimiento
+        etiqueta_fecha_nac = QLabel("Fecha de Nac.:", self)
+        etiqueta_fecha_nac.move(35, 300)
+        self.nacimiento = QLineEdit(self)
+        self.nacimiento.move(165, 300)
+        self.nacimiento.resize(200, 20)
+               
+        # Etiqueta sexo y radioButtons
+        etiqueta_sexo = QLabel("Sexo:", self)
+        etiqueta_sexo.move(35, 330)
+        rb_masculino = QRadioButton("masculino", self)
+        rb_masculino.move(100, 330)
+        rb_femenino = QRadioButton("femenino", self)
+        rb_femenino.move(210, 330)
+        rb_otro = QRadioButton("otro", self)
+        rb_otro.move(314, 330)
+        rb_grupo = QButtonGroup(self)
+        rb_grupo.addButton(rb_masculino)
+        rb_grupo.addButton(rb_femenino)
+        rb_grupo.addButton(rb_otro)
+                
         # Crea etiquetas de contraseña y confirmación de contraseña
         etiqueta_passwd = QLabel("Contraseña:", self)
-        etiqueta_passwd.move(35, 240)
+        etiqueta_passwd.move(35, 360)
         self.passwd = QLineEdit(self)
         self.passwd.setEchoMode(QLineEdit.Password)
-        self.passwd.move(165, 240)
+        self.passwd.move(165, 360)
         self.passwd.resize(200, 20)
         etiqueta_confirmar = QLabel("Confirmar:", self)
-        etiqueta_confirmar.move(35, 270)
+        etiqueta_confirmar.move(35, 390)
         self.confirmar_entrada = QLineEdit(self)
         self.confirmar_entrada.setEchoMode(QLineEdit.Password)
-        self.confirmar_entrada.move(165, 270)
+        self.confirmar_entrada.move(165, 390)
         self.confirmar_entrada.resize(200, 20)
         
         # botón de registro
         boton_registro = QPushButton("Registrarse", self)
-        boton_registro.move(100, 310)
+        boton_registro.move(100, 430)
         boton_registro.resize(200, 40)
         boton_registro.clicked.connect(self.confirmarRegistro)
 
