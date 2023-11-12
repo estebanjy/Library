343114f2b59e1793de8fa3b375348d1e17e7e89f
AguilarRaulRicardo <117322514+AguilarRaulRicardo@users.noreply.github.com>
2023-11-12 17:43:07 -0300

    Add files via upload

--- accionesLibro.py
+++ accionesLibro.py
@@ -1,20 +1,31 @@
 import sqlite3
+from libro import Libro
 from abc import ABC, abstractmethod
 
-
-class Acciones(ABC):
-    
+class Acciones(ABC):  
     def verLibrosDisponibles():
         conexion = sqlite3.connect("biblioteca.db")
         cursor = conexion.cursor()
-        libros = cursor.execute("""SELECT Titulo FROM Libros
+        libros = cursor.execute("""SELECT Imagen, Titulo, Autores, Genero, Editorial, Resumen FROM Libros
         WHERE IDUsuarioNormal IS NULL""")
+        resultado = libros.fetchall()
         cursor.close()
         conexion.close()
-        resultado = libros.fetchall()
         #aqui se tiene que crear algo para poder mostrarlo despues en el visual
         return resultado
-
+    
+    def verDatosGeneralesLibro(libro):
+        titulo = libro.titulo
+        autores = libro.autores
+        isbn = libro.ISBN
+        genero = libro.genero
+        editorial = libro.editorial
+        fechaPublicacion = libro.fechaPublicacion
+        resumen = libro.resumen
+        imagen = libro.imagen
+        usuarioPrestado = libro.usuarioPrestado
+    
+    
 class AccionesAdministrador(Acciones): 
     #hay que hacer algo para que las acciones de administrador esten enlasadas con una cuenta admin 
     def guardarlibro(libro):
@@ -35,40 +46,65 @@ class AccionesAdministrador(Acciones):
         cursor.close()
         conexion.close()
     
-    def verLibrosTomados(libros):
+    def verLibrosTomados():
         conexion = sqlite3.connect("biblioteca.db")
         cursor = conexion.cursor()
         libros = cursor.execute("""SELECT Titulo FROM Libros
         WHERE IDUsuarioNormal IS NOT NULL""")
+        resultado = libros.fetchall()
         cursor.close()
         conexion.close()
-        resultado = libros.fetchall()
         #aqui se tiene que crear algo para poder mostrarlo despues en el visual
         return resultado
-        
+    
+    def verHistorialPrestamosLibro(libro):
+        pass
         
 class AccionesUsuarioNormal(Acciones):
-    #estas acciones tienen que estar enlazadas a una cuenta admin
-    def pedirLibro(UsuarioNormal, libro):
-        #Aqui primero se tendria que mostrar al usuario todos los libros disponibles
+
+    def verLibrosTomados(IDUsuarioNormal):
         conexion = sqlite3.connect("biblioteca.db")
         cursor = conexion.cursor()
+        libros = cursor.execute("""SELECT Imagen, Titulo, Autores, Genero, Editorial, Resumen FROM Libros
+        WHERE IDUsuarioNormal IS ?""",(IDUsuarioNormal,))
+        libros = libros.fetchall()
+        cursor.close()
+        conexion.close()
+        #En esta parte no es una buena practica devolver una lista por lo cual hay que buscar una forma de devolver esto sin perjudicar esto
+        return libros
+        
+    #estas acciones tienen que estar enlazadas a una cuenta usuario
+    #aqui hay que desempaquetar el libro para que se pueda hacer la operacion
+    def pedirLibro(UsuarioNormal, libro):
+        #En esta parte hay que entregarle el objeto de UsuarioNormal si o si los datos del usuario de la base de datos
+        #Aqui primero se tendria que mostrar al usuario todos los libros disponibles
+        conexion = sqlite3.connect("biblioteca.db")
+        cursor = conexion.cursor() 
+        registro = cursor.execute("""SELECT HistorialPrestamos FROM Libros 
+        WHERE ISBN = ?""", (libro.ISBN, ))
+        resultado = registro.fetchone()
+        resultado = resultado[0] 
+        if resultado == None:
+            resultado = ""
+
         cursor.execute("""UPDATE Libros
-        SET IDUsuarioNormal = ? 
+        SET IDUsuarioNormal = ?, HistorialPrestamos = ?
         WHERE ISBN = ?
-        """,(UsuarioNormal.IDUsuarioNormal, libro.ISBN))
+        """,(UsuarioNormal.IDUsuarioNormal,resultado + UsuarioNormal.IDUsuarioNormal + ", ", libro.ISBN))
+        
         conexion.commit()
-        cursor.execute()
-        conexion.execute()
+        cursor.close()
+        conexion.close()
     
-    def devolverLibro(UsuarioNormal, libro):
-        #Aqui se necesita algo para que el usuartio pueda elegir que libro quiere devolver
+    #hay que crear una metodo que muestre todos los libros que tiene prestado el usuario 
+    def devolverLibro(libro):
+        #Aqui se necesita algo para que el usuario pueda elegir que libro quiere devolver 
         conexion = sqlite3.connect("biblioteca.db")
         cursor = conexion.cursor()
         cursor.execute("""UPDATE Libros
         SET IDUsuarioNormal = NULL 
-        WHERE ISBN = ?
-        """,(UsuarioNormal.IDUsuarioNormal, libro.ISBN))
+        WHERE ISBN = ?""",(libro.ISBN, ))
         conexion.commit()
-        cursor.execute()
-        conexion.execute()
+        cursor.close()
+        conexion.close()
+    
